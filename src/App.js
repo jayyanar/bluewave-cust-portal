e425002b67261933aa031b54b70932837f685d8a
Punith <punith.p@icloud.com>
2024-04-09 15:50:38 +0530

    new upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import config from "./amplifyconfiguration.json";
+import { uploadData } from "aws-amplify/storage";
+import "@aws-amplify/ui-react/styles.css";
+import "./App.css";
+
+// Configure Amplify
+Amplify.configure(config);
+
+function App({ signOut, user }) {
+  const [fileData, setFileData] = useState(null);
+  const [fileUploadStatus, setFileUploadStatus] = useState(false);
+
+  // Function to handle file upload
+  const uploadFile = async () => {
+    try {
+      const result = await uploadData({
+        key: fileData.name,
+        data: fileData,
+      }).result;
+      setFileUploadStatus(true);
+      const fileInput = document.querySelector(".file-input");
+      if (fileInput) {
+        fileInput.value = "";
+      }
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="wave">
+      <div className="App">
+        <h1>Welcome to Bluewave Cust</h1>
+        <hr />
+        <button className="sign-out-btn" onClick={signOut}>
+          Sign out
+        </button>
+        <div className="file-input-container">
+          <input
+            type="file"
+            className="file-input"
+            onChange={(e) => setFileData(e.target.files[0])}
+          />
+          <div className="file-input-icon">Choose File</div>
+        </div>
+        <div>
+          <button className="file-upload-btn" onClick={uploadFile}>
+            Upload
+          </button>
+        </div>
+        <div className="upload-status">
+          {fileUploadStatus && `${fileData.name} was uploaded successfully`}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
